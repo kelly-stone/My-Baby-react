19b155185f9dd69dbb3a4d1dc3fe355b75c36bbb
Kelly <kellyandstone@gmail.com>
2018-11-11 15:27:20 +1300

    addDiary and onChange was missing

--- client/components/Diary.jsx
+++ client/components/Diary.jsx
@@ -21,14 +21,14 @@ class Diary extends React.Component{
             <div>
                 <h2>You are my little angel</h2>
                 <DiaryAddForm addDiary={this.addDiary}/>
-               {/* {this.state.diarys.map(diary =>{
+               {this.state.diarys.map(diary =>{
                     <div>
                     <h3>{diary.Name}</h3>,
                     <h3>{diary.Date}</h3>,
                     <h3>{diary.Location}</h3>,
                     <h3>{diary.Subject}</h3>,
                     <h3>{diary.Img}</h3>
-                    </div> */}
+                    </div> 
                 })} 
             </div>
         )
