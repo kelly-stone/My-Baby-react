f5f500fdb3837960aaf732666a78e411640e860d
Kelly <kellyandstone@gmail.com>
2018-11-11 14:37:01 +1300

    add form

--- client/components/Diary.jsx
+++ client/components/Diary.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 
-// import DiaryAddForm from './DiaryAddForm'
+import DiaryAddForm from './DiaryAddForm'
 
 class Diary extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            // diary:[{Name:"", Date:"", Location:"",Suject:"", Img:""}]
+            Diary:[{Name:"Jadon", Date:"10/06/2016", Location:"Home",Suject:"1st Birthday", Img:"/../../IMG_2176.jpg"}]
         }
-       
+       this.addDiary = this.addDiary.bind(this)
 
         }
-        addDiary(diary) {
-            const diarys = this.state.diarys
-            diarys.push(diary)
-            this.setState({diarys:diary})
+        addDiary(Diary) {
+            const Diarys = this.state.Diarys
+            Diarys.push(Diary)
+            this.setState({Diarys:Diarys})
     }
     render(){
         return(
             <div>
                 <h2>You are my little angel</h2>
-                {/* <DiaryAddForm /> */}
+                <DiaryAddForm />
+                {/* {this.state.diarys.map(diary =>{
+                    <div>
+                    <h3>{diary.Name}</h3>,
+                    <h3>{diary.Date}</h3>,
+                    <h3>{diary.Location}</h3>,
+                    <h3>{diary.Subject}</h3>,
+                    <h3>{diary.Img}</h3>
+                    </div>
+                })} */}
             </div>
         )
     }
--- client/components/DiaryAddForm.jsx
+++ client/components/DiaryAddForm.jsx
@@ -1,5 +1,42 @@
 import React from 'react'
 
+class DiaryAddForm extends React.Component{
+    constructor(props) {
+        super(props)
+        this .state={
+            Diary:{Name:"", Date:"", Location:"",Suject:"", Img:""}
+        }
+this.submit = this.submit.bind(this)
+this.updateDetail = this.updateDetail.bind(this)
+}
+
+submit(e){
+    e.preventDefault()
+    const Diary = this.state.Diary
+    console.log({Diary})
+    this.props.addDiary(Diary)
+}
+
+updateDetail(e){
+    const Diary = this.state.Diary
+    Diary[e.target.name] = e.target.value
+    this.setState({Diary})
+}
+    render(){
+    return(
+        <form onSubmit = {this.submit}>
+            <input placeholder="child name" type="text" name="Name"/>
+            <input placeholder="dd/mm/yy" type="text" name="Date"/>
+            <input placeholder="where" type="text" name="Location"/>
+            <input placeholder="what happen" type="text" name="Subject"/>
+            <input placeholder="photo" type="text" name="Img"/>
+            <input type="submit" value="add me"/>
+        </form>
+    )
+
+
+
+    } 
 
 
 
@@ -8,17 +45,10 @@ import React from 'react'
 
 
 
+    
 
 
-
-
-
-
-
-
-
-
-
+}
 
 
 export default DiaryAddForm
